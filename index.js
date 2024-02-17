885d8f47340225936a0191542d01b3f4dbf96ed0
Peter Baranec <peter.baranec.devtech@gmail.com>
2024-02-17 12:01:40 +0100

    eventlistener for click, array myLeads and rendering li items

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-function saveLead() {
-  console.log("click");
+let myLeads = ["awesome", "epic", "lol"];
+const inputBtn = document.getElementById("input-btn");
+const inputEl = document.getElementById("input-el");
+const ulEL = document.getElementById("ul-el");
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  console.log(myLeads);
+});
+
+for (let i = 0; i < myLeads.length; i++) {
+  ulEL.innerHTML += "<li>" + myLeads[i] + "</li>";
 }
