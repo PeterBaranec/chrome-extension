2cb412b1495832367582f52b53113023ae85b21c
Peter Baranec <peter.baranec.devtech@gmail.com>
2024-02-20 16:33:22 +0100

    final app

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let myLeads = [];
 const inputBtn = document.getElementById("input-btn");
 const deleteBtn = document.getElementById("delete-btn");
+const saveBtn = document.getElementById("save-btn");
 const inputEl = document.getElementById("input-el");
 const ulEL = document.getElementById("ul-el");
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
@@ -11,12 +12,20 @@ if (leadsFromLocalStorage) {
   render(myLeads);
 }
 
+saveBtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
+});
+
 function render(leads) {
   let listItems = "";
   for (let i = 0; i < leads.length; i++) {
     listItems += `
     <li>
-      <a target='_blank' href='https://${leads[i]}'>${leads[i]}</a>
+      <a target='_blank' href='${leads[i]}'>${leads[i]}</a>
     </li>`;
   }
   ulEL.innerHTML = listItems;
